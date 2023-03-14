ffa2f6d37c873be1fc8694524268da6d6bcb7b49
yshmarov <yshmarov@gmail.com>
2023-03-14 21:38:08 +0100

    generate token on create

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -1,2 +1,12 @@
 class ApiToken < ApplicationRecord
+  validates :name, presence: true
+  validates :token, presence: true
+
+  before_validation :generate_token, on: :create
+
+  private
+
+  def generate_token
+    self.token = Digest::MD5.hexdigest(SecureRandom.hex)
+  end
 end
